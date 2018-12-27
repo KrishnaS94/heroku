b432de55b62531d78d4a1d181a1adaa2ecd2f93e
manoj485 <manoj.savaram@rapidqube.com>
2018-12-27 15:20:19 +0530

    upd

--- app.js
+++ app.js
@@ -30,11 +30,11 @@ const logger = log4js.getLogger('Aman_project');
 
 con.connectionCheck;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// })
 
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'pug');
